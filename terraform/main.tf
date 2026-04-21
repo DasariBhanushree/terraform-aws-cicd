a68a44499d9daa7067425136daf0f77365f9d5b9
DasariBhanushree <dasaribhanushree@gmail.com>
2026-04-21 14:22:06 +0000

    backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-demo-bucket-990765"
+    bucket = "my-terraform-test-bucket-990765"
     key    = "terraform.tfstate"
     region = "ap-south-1"
   }
