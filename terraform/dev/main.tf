fbbab3dd0dcb399f41d18f253d65d71ab1427ddc
DasariBhanushree <dasaribhanushree@gmail.com>
2026-04-22 16:15:53 +0530

    Update main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-243-bucket-990765"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
 module "ec2_dev" {
   source = "../modules/ec2"
 }
